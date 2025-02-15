4beb8c3f919d486ceacee7f8b8baaddd5d1a90e6
Luiz Carneiro <luiz@carneiro.dev>
2025-02-15 18:35:33 +0100

    Almost finished. The math can be simplified.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Slider } from './components/Slider'
+import styles from './App.module.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [percentageValue, setPercentageValue] = useState(67)
+  const [proportionalValue, setProportionalValue] = useState(0.33)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={styles.container}>
+      <Slider
+        label="Percentage Label"
+        min={0}
+        max={100}
+        step={1}
+        value={percentageValue}
+        unit="%"
+        onChange={setPercentageValue}
+      />
+      
+      <Slider
+        label="Proportional Label"
+        min={0}
+        max={1}
+        step={0.01}
+        value={proportionalValue}
+        onChange={setProportionalValue}
+      />
+    </div>
   )
 }
-
-export default App
